ea6062118d2c0a4fb45eb52b96f06f16d238cea1
salmane_mhb <smouhib.ieu2023@student.ie.edu>
2025-10-22 00:10:29 +0200

    Add missing LanguageToggle component

---
+++ luma-frontend/src/components/LanguageToggle.tsx
@@ -0,0 +1,35 @@
+import { useTranslation } from 'react-i18next';
+import { Button } from './ui/button';
+import { Languages } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+
+export const LanguageToggle = () => {
+  const { i18n } = useTranslation();
+
+  const toggleLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Languages className="h-5 w-5" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => toggleLanguage('en')}>
+          English
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => toggleLanguage('es')}>
+          Español
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
